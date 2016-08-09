6f1ebfc619fc3200abf153e5c3b656d35b394755
= <=>
2016-08-09 17:22:29 +0500

    Clear repo

--- scripts/toggle_tray.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import subprocess
-
-output = subprocess.getoutput('ps -A')
-if 'tint2' in output:
-    subprocess.run( ["killall", "tint2"] )
-else:
-    subprocess.run( "tint2" )
